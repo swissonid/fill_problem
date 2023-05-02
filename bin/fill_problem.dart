77f92bcc475063078ef2eac0d2e4c82c0212dd4b
Patrice Müller <mueller.patrice@gmail.com>
2023-05-02 16:01:10 +0200

    init commit

---
+++ bin/fill_problem.dart
@@ -0,0 +1,5 @@
+import 'package:fill_problem/fill_problem.dart' as fill_problem;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fill_problem.calculate()}!');
+}
---
+++ test/fill_problem_test.dart
@@ -0,0 +1,8 @@
+import 'package:fill_problem/fill_problem.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
